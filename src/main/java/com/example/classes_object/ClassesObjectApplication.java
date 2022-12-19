8863194fd87015d9565003d059e166c43f222ebc
Archi903 <artpele@yandex.ru>
2022-12-19 13:20:25 +0300

    Initial commit

---
+++ src/main/java/com/example/classes_object/ClassesObjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.classes_object;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassesObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassesObjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classes_object/ClassesObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classes_object;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
